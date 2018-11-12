0e97203a49084a5e7aa0dd3f7c7b11f822820bd5
Michael Kardjian <makardjian@gmail.com>
2018-11-12 12:08:36 -0800

    completed the Grub super class and bee sub class

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
-class Bee {
-  // TODO..
+class Bee extends Grub{
+  
+  constructor() {
+  	super();
+  	this.age = 5;
+  	this.color = 'yellow';
+  	// this.food is inhereted from Grub
+  	//this.eat is inhereted from Grub
+  	this.job = 'Keep on growing';
+
+  }
 };
